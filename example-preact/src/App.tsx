bf92b1448f6111f2fc194d2d68d5ce1f7bcded30
Mateusz Wszeborowski <wszeborowskimateusz@gmail.com>
2025-06-18 11:29:47 -0700

    Release v0.3.2 (#237)

--- example-preact/src/App.tsx
+++ example-preact/src/App.tsx
@@ -47,7 +47,7 @@ export default function App() {
               Javascript application. <b>No React required.</b> (
               <a
                 className="text-blue-500 underline"
-                href="https://hellocsv.mintlify.app/v0.3.1/get-started/usage#react"
+                href="https://hellocsv.mintlify.app/v0.3.2/get-started/usage#react"
                 target="_blank"
                 rel="noreferrer noopener"
               >
