0e0538e0e049b5a5cdd22b11929168e3fc17f6f8
rvhoyt <rvhoyt@gmail.com>
2025-07-02 13:50:57 -0400

    Fix infinite loop in virtualized rows (#244)
    * Fix infinite loop in virtualized rows
    
    Do not recalculate in the render causing state changes and infinite loop
    
    * Fix formatting
    
    ---------
    
    Co-authored-by: Mateusz Wszeborowski <wszeborowskimateusz@gmail.com>

--- src/sheet/components/SheetDataEditorTable.tsx
+++ src/sheet/components/SheetDataEditorTable.tsx
@@ -13,7 +13,7 @@ import { useTranslations } from '@/i18';
 import { findRowIndex } from '../utils';
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid';
 import { useVirtualizer } from '@tanstack/react-virtual';
-import { RefObject } from 'preact/compat';
+import { RefObject, useCallback } from 'preact/compat';
 import { CHECKBOX_COLUMN_ID, ESTIMATED_ROW_HEIGHT } from '@/constants';
 
 interface Props {
@@ -88,6 +88,13 @@ export default function SheetDataEditorTable({
         ]
       : [0, 0];
 
+  const measureRef = useCallback(
+    (node: HTMLElement | null) => {
+      if (node) rowVirtualizer.measureElement(node);
+    },
+    [rowVirtualizer]
+  );
+
   return (
     <table
       className="w-full table-fixed border-separate border-spacing-0"
@@ -161,11 +168,7 @@ export default function SheetDataEditorTable({
           <td style={{ height: paddingTop }} />
         </tr>
         {visibleRows.map(({ row, index }) => (
-          <tr
-            key={row.id}
-            data-index={index}
-            ref={(node) => rowVirtualizer.measureElement(node)}
-          >
+          <tr key={row.id} data-index={index} ref={measureRef}>
             {row.getVisibleCells().map((cell, cellIndex) => {
               if (cell.column.id === CHECKBOX_COLUMN_ID) {
                 return (
